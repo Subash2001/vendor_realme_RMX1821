2fac6e8f7ca2656b303f91164780d00323be5885
Christopher N. Hesse <raymanfx@gmail.com>
2018-11-26 19:28:54 +0100

    RMX1821: Hexedit mtk-ril to fix incoming calls
    Taken from: https://github.com/phhusson/treble_experimentations/issues/57#issuecomment-416998086
    
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- RMX1821-vendor.mk
+++ RMX1821-vendor.mk
@@ -21,4 +21,5 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1821/proprietary/bin/kpoc_charger:$(TARGET_COPY_OUT_SYSTEM)/bin/kpoc_charger \
     vendor/realme/RMX1821/proprietary/etc/init/kpoc_charger.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/kpoc_charger.rc \
     vendor/realme/RMX1821/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
-    vendor/realme/RMX1821/proprietary/lib/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsysenv_system.so
+    vendor/realme/RMX1821/proprietary/lib/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsysenv_system.so \
+    vendor/realme/RMX1821/proprietary/lib64/libmtk-ril.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmtk-ril.so
