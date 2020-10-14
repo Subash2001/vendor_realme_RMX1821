0785d911ba2dcc277d28a07d1a31a276e5a9cabd
Maanush Putcha <p.maanush@gmail.com>
2020-10-14 12:46:24 +0530

    RMX1821: Also add libsysenv_system.so from lib64
    Signed-off-by: Maanush Putcha <p.maanush@gmail.com>

--- RMX1821-vendor.mk
+++ RMX1821-vendor.mk
@@ -22,4 +22,5 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1821/proprietary/etc/init/kpoc_charger.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/kpoc_charger.rc \
     vendor/realme/RMX1821/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
     vendor/realme/RMX1821/proprietary/lib/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsysenv_system.so \
+    vendor/realme/RMX1821/proprietary/lib64/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libsysenv_system.so \
     vendor/realme/RMX1821/proprietary/lib64/libmtk-ril.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libmtk-ril.so
