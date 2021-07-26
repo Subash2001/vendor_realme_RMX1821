050c6abd578823f58dc4d3cef0307b15777e17fc
Sonal Singh <sonal.singh.19993@gmail.com>
2021-07-26 21:32:18 +0530

    RMX1821: Add Dirac libs and configs
    * For RealmeDirac
    
    Signed-off-by: improbir <ghosh_probir@yahoo.com>
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: CyberTechWorld <cybertechworld.ctw@gmail.com>
    Signed-off-by: subash2001 <subashselvam0@gmail.com>

--- RMX1821-vendor.mk
+++ RMX1821-vendor.mk
@@ -12,4 +12,8 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1821/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
     vendor/realme/RMX1821/proprietary/lib/libsysenv_system.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libsysenv_system.so \
     vendor/realme/RMX1821/proprietary/product/vendor_overlay/29/lib/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libdpframework.so \
-    vendor/realme/RMX1821/proprietary/product/vendor_overlay/29/lib64/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/libdpframework.so
+    vendor/realme/RMX1821/proprietary/product/vendor_overlay/29/lib64/libdpframework.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib64/libdpframework.so \
+    vendor/realme/RMX1821/proprietary/product/vendor_overlay/29/etc/audio_effects.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/etc/audio_effects.xml \
+    vendor/realme/RMX1821/proprietary/product/vendor_overlay/29/etc/diracmobile.config:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/etc/diracmobile.config \
+    vendor/realme/RMX1821/proprietary/product/vendor_overlay/29/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libDiracAPI_SHARED.so \
+    vendor/realme/RMX1821/proprietary/product/vendor_overlay/29/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/soundfx/libdirac.so 
